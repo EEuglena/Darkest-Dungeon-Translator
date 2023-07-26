2d5c463a1acb4f9733692a161d29aacb00ffe6b6
EEuglena <halfmoon51525@gmail.com>
2023-07-26 15:42:13 +0900

    Fix(Navigator)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import styles from "../styles/Header.module.css";
 const Header = () => {
 	return (
 		<div className={styles.header}>
-			<img src={process.env.PUBLIC_URL + "logo.png"} alt="" />
+			<img src={process.env.PUBLIC_URL + "/logo.png"} alt="" />
 			<h1>다키스트 던전 번역 지원툴</h1>
 		</div>
 	);
--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import styles from "../styles/Navigator.module.css";
 
 const Navigator = () => {
 	return (
 		<div className={styles.container}>
-			<a href="/">ABOUT</a>
-			<a href="/xml">XML → XLSX</a>
-			<a href="/xlsx">XLSX → XML</a>
-			<a href="/publish">⛔PUBLISH🛠</a>
+			<Link to={"/"}>ABOUT</Link>
+			<Link to={"/xml"}>XML → XLSX</Link>
+			<Link to={"/xlsx"}>XLSX → XML</Link>
+			<Link to={"/publish"}>⛔PUBLISH🛠</Link>
 		</div>
 	);
 };
